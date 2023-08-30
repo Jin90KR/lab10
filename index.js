9a3271b4e02792de099d6118015bf9996e0200ca
hwanjin lee <hwanjinlee90@gmail.com>
2023-08-30 15:44:21 -0700

    update: closebox

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var openBtn = document.getElementById("openbutton");
 var whatsYourName = prompt("What is your name?");
 var openModal = document.getElementById("openmodal");
 var modalName = document.getElementById("modalname");
+var modalClose = document.getElementById("modalclose")
 
 console.log(whatsYourName);
 
@@ -13,6 +14,8 @@ openBtn.addEventListener('click', ()=>{
     openModal.classList.add('showmodal')
 })
 
-modalName.textContent = whatsYourName
-console.log(modalName.textContent)
+modalClose.addEventListener('click', ()=>{
+    openModal.classList.remove('showmodal')
+})
 
+modalName.textContent = "당신의 이름은  " + whatsYourName + "  입니다 Coding Hakdang에 오신 것을 환영합니다."
